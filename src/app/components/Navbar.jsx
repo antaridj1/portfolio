7d51be3acf5583fbb98187fda3f16ecc8de8c3ef
antari.appkey <antari.appkey@gmail.com>
2024-08-01 21:03:45 +0800

    fix: navbar improvement

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -14,7 +14,7 @@ export default function Navbar() {
 			<nav className="bg-white w-full z-20 top-0 start-0">
 				<div className="flex flex-wrap items-center justify-between py-4 px-2">
 					<a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-						<span className="self-center dark:text-white">Antari Dwijayanti (A - D)</span>
+						<span className="self-center">Antari Dwijayanti (A - D)</span>
 					</a>
 					
 					<div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
@@ -42,7 +42,7 @@ export default function Navbar() {
 							data-collapse-toggle="navbar-sticky"
 							type="button"
 							onClick={handleNavbar}
-							className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+							className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none"
 							aria-controls="navbar-sticky"
 							aria-expanded={isNavbarOpen}
 						>
@@ -69,24 +69,24 @@ export default function Navbar() {
 						className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isNavbarOpen ? "block" : "hidden"}`}
 						id="navbar-sticky"
 					>
-						<ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+						<ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
 							<li>
-							<a href="/" className="block md:hidden py-2 px-3 text-black rounded md:bg-transparent md:text-gray-700 md:p-0 md:dark:text-gray-500" aria-current="page">
+							<a href="/" className="block md:hidden py-2 px-3 text-black rounded md:bg-transparent md:text-gray-700 md:p-0" aria-current="page">
 								Home
 							</a>
 							</li>
 							<li>
-							<a href="#works" className="block md:hidden py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+							<a href="#works" className="block md:hidden py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
 								Works
 							</a>
 							</li>
 							<li>
-							<a href="#about" className="block md:hidden py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+							<a href="#about" className="block md:hidden py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
 								About
 							</a>
 							</li>
 							<li>
-							<a href="#contact" className="block md:hidden py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+							<a href="#contact" className="block md:hidden py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
 								Contact
 							</a>
 							</li>
