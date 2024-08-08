fadc1722e23ce8d35b1d2b84938945f8d99f095e
antari.appkey <antari.appkey@gmail.com>
2024-08-08 21:13:46 +0800

    feat: add portfolio project

--- src/app/data/datawork.jsx
+++ src/app/data/datawork.jsx
@@ -1,6 +1,21 @@
 const dataWorks = [
+  {
+    id: 1,
+    title: 'Web Developer Portfolio',
+    subtitle: 'Personal Portfolio Website',
+    workType: 'Design & Develop',
+    projectType: "Web Development",
+    thumbnail: "portfolio_thumb.png",
+    link: "https://www.antaridwijayanti.com",
+    image: "portfolio.png",
+    detail: {
+      overview: "This is a minimalist portfolio website showcasing a curated selection of my work, built using React.js, Next.js, and Tailwind CSS. The goal was to create a clean and functional digital space that allows users to explore my portfolio without distractions. Each project is presented in a straightforward manner, emphasizing content and providing an intuitive user experience.",
+      challenges: "The primary challenge in developing this website was achieving a balance between simplicity and effectiveness. Given that the site features a significant amount of text, it was crucial to design an interface that keeps the focus on the main content while maintaining readability. Ensuring that the website remains uncluttered was key to providing users with a comfortable browsing experience. Additionally, the site needed to be fully responsive, adapting seamlessly across different screen sizes, as many users are likely to access the portfolio on mobile devices",
+      results: "The design phase was completed in Figma over three days, focusing on creating a visually appealing and functional layout. I developed the site over a week, my first time using React.js and Next.js. Despite the learning curve, I efficiently implemented the design using Tailwind CSS, ensuring consistency across the site. The final product is a fully responsive website that achieved a 97% performance score on GTMetrix, reflecting its optimization and fast loading times. The minimalist design successfully highlights the portfolio content, offering a clean, engaging browsing experience across all devices"
+    }
+  },
     {
-      id: 1,
+      id: 2,
       title: 'Bali Bagus Rentcar',
       subtitle: 'Car Rent Website',
       workType: 'Design & Develop',
@@ -15,7 +30,7 @@ const dataWorks = [
       }
     },
     {
-      id: 2,
+      id: 3,
       title: 'MenuMate',
       subtitle: 'QR Code Menu Ordering System',
       workType: 'Develop',
@@ -27,21 +42,6 @@ const dataWorks = [
         overview: "Menu Mate is a web application for restaurants that want to offer a QR code menu to their customers. Customers simply scan the QR code on the table and are redirected to the website, where they can browse the menu and order food directly. This is an internal project by APPKEY, available for purchase on Codester.",
         challenges: "In this project, I was responsible for handling the frontend development. I had to mplemented all the details and interactive features to precisely match the design provided by the designer while also integrating APIs from the backend developer. Since the website displays numerous products with large images, enhancing its performance was a key challenge. Additionally, as the website is primarily used by customers on their smartphones, ensuring it was fully responsive was essential.",
         results: "MenuMate was developed using tools such as HTML, CSS, Bootstrap, jQuery, PHP, and Laravel. I completed the frontend development in less than three weeks and chose to use Laravel Blade for the development. To enhance performance, I utilized jQuery to fetch data asynchronously from the API. For the design, I used Bootstrap and customized it with plain CSS to match the provided design, ensuring it was responsive on smartphones, tablets, and PC screens."
-      }
-    },
-    {
-      id: 3,
-      title: 'OracleAI',
-      subtitle: 'Forecasting Mobile App',
-      workType: 'Develop',
-      projectType: "Mobile Development",
-      thumbnail: "oracleai_thumb.png",
-      link: "https://play.google.com/store/apps/details?id=com.appkey.oracleai",
-      image: "oracleai.png",
-      detail: {
-        overview: "Developed using Flutter, OracleAI is an innovative mobile application that predicts users' destinies based on their card selections. Leveraging the power of the GPT API for advanced algorithms, OracleAI provides personalized and insightful predictions.",
-        challenges: "Developing a mobile application with no prior experience in mobile development was one of the biggest challenges of my career. I spent two weeks learning Flutter and then immediately began development. Additionally, I worked with APIs provided by the backend developer, using Dart and Flutter for the first time. Despite the challenges, I was very grateful for the opportunity to learn new front-end skills.",
-        results: "OracleAI was released after three months of development and testing, and has been downloaded over 100 times on the Play Store. Built using Dart and Flutter, the app is responsive and accessible on both smartphones and tablets, closely matching the provided design."
       }
     },
     {
@@ -59,6 +59,21 @@ const dataWorks = [
         results: "T-Bank was released in 2023 and has over 3,000 registered users. One of the most challenging tasks I successfully completed in this system was fixing an overloaded datatable issue on the front end, which handled over 2,000 records, within two days. In addition to bug fixes, my contributions included changing the main banner to a video, implementing minor display adjustments, adding a video upload feature for member profiles, and developed a free trial chat feature for unpaid male members."
       }
     },
+        // {
+    //   id: 3,
+    //   title: 'OracleAI',
+    //   subtitle: 'Forecasting Mobile App',
+    //   workType: 'Develop',
+    //   projectType: "Mobile Development",
+    //   thumbnail: "oracleai_thumb.png",
+    //   link: "https://play.google.com/store/apps/details?id=com.appkey.oracleai",
+    //   image: "oracleai.png",
+    //   detail: {
+    //     overview: "Developed using Flutter, OracleAI is an innovative mobile application that predicts users' destinies based on their card selections. Leveraging the power of the GPT API for advanced algorithms, OracleAI provides personalized and insightful predictions.",
+    //     challenges: "Developing a mobile application with no prior experience in mobile development was one of the biggest challenges of my career. I spent two weeks learning Flutter and then immediately began development. Additionally, I worked with APIs provided by the backend developer, using Dart and Flutter for the first time. Despite the challenges, I was very grateful for the opportunity to learn new front-end skills.",
+    //     results: "OracleAI was released after three months of development and testing, and has been downloaded over 100 times on the Play Store. Built using Dart and Flutter, the app is responsive and accessible on both smartphones and tablets, closely matching the provided design."
+    //   }
+    // },
   ];
   
   export default dataWorks;
